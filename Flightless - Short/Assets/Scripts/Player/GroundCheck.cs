1fc237f58199c59d58cdc0094721f7ea44ba74f7
Benjamin <contact@flightlessgames.net>
2020-07-04 01:26:09 -0500

    Movement and Jump in PlayerMotor
    Corrected simple movement directions (left & right) by changing movement input into vector2 instead of int and event. Player now moves left and right and flies up.
    GroundCheck for 2D cannot use Physics.OverlapBox, need to look into 2D appropriate check for collisions to determine isGrounded state for player jump.

---
+++ Flightless - Short/Assets/Scripts/Player/GroundCheck.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider2D))]
+public class GroundCheck : MonoBehaviour
+{
+    private Collider2D _collider;
+
+    private void Awake()
+    {
+        _collider = GetComponent<Collider2D>();
+    }
+
+    public bool GroundedCheck()
+    {
+        Vector2 transform2D = new Vector2(transform.position.x, transform.position.y);
+        Vector2 extents2D = new Vector2(_collider.bounds.extents.x, _collider.bounds.extents.y);
+        /*
+        Collider2D[] collisions = Physics2D.OverlapBox(transform2D, extents2D, 0f);//BUGFIX
+        Debug.Log(collisions.Length);
+        
+        foreach (Collider collider in collisions){
+            Debug.Log(collider.gameObject.name + ": " + collider.gameObject.tag);
+            if (collider.gameObject.CompareTag("Terrain"))
+            {
+                return true;
+            }
+        }
+        */
+        return false;
+    }
+}
---
+++ Flightless - Short/Assets/Scripts/Player/PlayerMotor_2D.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerMotor_2D: MonoBehaviour
+{
+    Player_Action controls = null;
+    Rigidbody2D rb2d = null;
+
+    [Header("Required")]
+    [SerializeField] private GroundCheck _groundCheck = null;
+
+    private enum Direction
+    {
+        Left = -1,
+        None = 0,
+        Right = 1
+    };
+
+    [Header("Settings")]
+    [SerializeField] private float _moveSpeed = 10f;
+
+    private Vector2 _movementInput = Vector2.zero;
+    private Direction _direction = Direction.None;
+
+
+    private void Awake()
+    {
+        controls = new Player_Action();
+        controls.Player_Movement.Move.performed += ctx => _movementInput = ctx.ReadValue<Vector2>();
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    private void OnEnable()
+    {
+        controls.Enable();
+    }
+
+    private void FixedUpdate()
+    {
+        _direction = (Direction)_movementInput.x;
+        Debug.Log(_movementInput + ": Moving " + _direction);
+        MovePlayer();
+        JumpPlayer();
+    }
+
+    private void MovePlayer()
+    {
+        rb2d.velocity = new Vector2(_movementInput.x * _moveSpeed, rb2d.velocity.y);
+    }
+
+    private void JumpPlayer()
+    {
+        if(_movementInput.y > 0)
+        {
+            Debug.Log("Jump Check");
+            //Layered if statements to reduce Grounded Collision polling (using Physics.OverlapBox; expensive)
+            bool isGrounded = _groundCheck.GroundedCheck();
+            if (isGrounded)
+            {
+                Debug.Log("Jump Success");
+            }
+            //AFTER BUGFIX IN GROUNDCHECK, MOVE INTO IF STATEMENT
+            rb2d.velocity = new Vector2(rb2d.velocity.x, _moveSpeed);
+        }
+    }
+}
